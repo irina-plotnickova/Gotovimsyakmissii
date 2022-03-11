31496b1be58c593247feb83a074290330154f393
irina-plotnickova <94181763+irina-plotnickova@users.noreply.github.com>
2022-03-11 17:21:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ def index():
     return render_template('base.html', title='Заготовка')
 
 
+@app.route('/training/<prof>', methods=['GET', 'POST'])
+def training(prof):
+    if str(prof) in ['инженер', 'строитель']:
+        print('ok')
+        return render_template('ingener.html', title='Заготовка')
+    else:
+        return render_template('science.html', title='Заготовка')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
